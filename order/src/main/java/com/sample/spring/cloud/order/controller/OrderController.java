b904127dc2a804b79a18e2bccc28bbee700f532a
kouzie <kgy1996@naver.com>
2020-06-30 19:17:50 +0900

    remove contract test source

--- order/src/main/java/com/sample/spring/cloud/order/controller/OrderController.java
+++ order/src/main/java/com/sample/spring/cloud/order/controller/OrderController.java
@@ -7,7 +7,6 @@ import com.sample.spring.cloud.order.client.CustomerClient;
 import com.sample.spring.cloud.order.client.ProductClient;
 import com.sample.spring.cloud.order.dto.Account;
 import com.sample.spring.cloud.order.dto.Customer;
-import com.sample.spring.cloud.order.dto.Product;
 import com.sample.spring.cloud.order.message.AccountOrder;
 import com.sample.spring.cloud.order.message.OrderSender;
 import com.sample.spring.cloud.order.message.ProductOrder;
@@ -20,8 +19,6 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
 
 
 @Slf4j
@@ -30,45 +27,20 @@ import java.util.Optional;
 public class OrderController {
     private final ObjectMapper mapper;
     private final OrderService orderService;
-    private final ProductClient productClient;
     private final AccountClient accountClient;
     private final CustomerClient customerClient;
     private final OrderSender orderSender;
 
-    @PostMapping
-    public Order prepare(@RequestBody Order order) throws JsonProcessingException {
-        List<Product> products = productClient.findByIds(order.getProductIds());
-        log.info("Products found: {}", mapper.writeValueAsString(products));
-
-        Customer customer = customerClient.findByIdWithAccounts(order.getCustomerId());
-        log.info("Customer found: {}", mapper.writeValueAsString(customer));
-
-        int price = products.stream().mapToInt(Product::getPrice).sum();
-        final int priceDiscounted = priceDiscount(price, customer); // 해당 customer 주문횟수만큼 discount
-        log.info("Discounted price: {}", mapper.writeValueAsString(Collections.singletonMap("price", priceDiscounted)));
-        Optional<Account> account = customer.getAccounts().stream()
-                .filter(a -> (a.getBalance() > priceDiscounted)).findFirst();
-        if (account.isPresent()) {
-            order.setAccountId(account.get().getId());
-            order.setStatus(OrderStatus.ACCEPTED);
-            order.setPrice(priceDiscounted);
-            log.info("Account found: {}", mapper.writeValueAsString(account.get()));
-        } else {
-            order.setStatus(OrderStatus.REJECTED);
-            log.info("Account not found: {}", mapper.writeValueAsString(customer.getAccounts()));
-        }
-        return orderService.save(order);
-    }
 
     // OrderSender 를 통해 order 를 메세지 브로커로 전달. account-service 가 받아 처리
-    /*@PostMapping
+    @PostMapping
     public Order process(@RequestBody Order order) throws JsonProcessingException {
-        order = orderService.add(order);
+        order = orderService.save(order);
         log.info("Order saved: {}", mapper.writeValueAsString(order));
         boolean isSent = orderSender.send(order);
         log.info("Order send: {}", mapper.writeValueAsString(Collections.singletonMap("isSent", isSent)));
         return order;
-    }*/
+    }
 
     @StreamListener(value = ProductOrder.INPUT)
     public void receiveProductOrder(Order order) throws JsonProcessingException {
@@ -81,7 +53,7 @@ public class OrderController {
         }
     }
 
-//    @StreamListener(value = AccountOrder.INPUT, condition = "headers['processor']=='account'")
+    //    @StreamListener(value = AccountOrder.INPUT, condition = "headers['processor']=='account'")
     @StreamListener(value = AccountOrder.INPUT)
     public void receiveAccountOrder(Order order) throws JsonProcessingException {
         log.info("Order receiveAccountOrder: {}", mapper.writeValueAsString(order));
